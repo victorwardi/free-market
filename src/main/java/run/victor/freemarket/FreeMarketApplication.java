fbbb2098e2462c000af36a26b8bc766c2467b92b
Victor Wardi <victorcaetano@live.com>
2020-09-01 08:46:01 -0300

    Initial commit

---
+++ src/main/java/run/victor/freemarket/FreeMarketApplication.java
@@ -0,0 +1,15 @@
+package run.victor.freemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class FreeMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeMarketApplication.class, args);
+    }
+
+}
